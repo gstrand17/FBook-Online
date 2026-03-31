5fd1f2e721a755d7dd10362bdeaa717b3fc04152
MonishB123 <monishbeegamudre@gmail.com>
2026-03-31 15:56:37 -0400

    removed logout button

--- pages/residence_activity.php
+++ pages/residence_activity.php
@@ -54,7 +54,6 @@
                         <li><a class="dropdown-item" href="#">Action</a></li>
                     </ul>
                 </div>
-                <a class="btn btn-primary" href="#" role="button">Logout</a>
             </div>
         </nav>
 		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/js.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
