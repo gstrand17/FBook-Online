a371fafabf430f3d31790770c8b6bae9ddef7864
Daniel-cs-uf <dM@0610010566>
2026-03-31 12:35:10 -0400

    more locations

--- seed.sql
+++ seed.sql
@@ -8,9 +8,16 @@ INSERT INTO location (location_id, place_name, latitude, longitude, address, map
   (7, 'Archway', 29.6505, -82.3428, '300 SW 13th St, Gainesville, FL 32611', NULL),
   (8, 'Southwest Recreation Center', 29.6384, -82.3683, '3150 Hull Rd, Gainesville, FL 32611', NULL),
   (9, 'UF Student Health Care Center', 29.6500, -82.3500, '2140 Stadium Rd, Gainesville, FL 32611', NULL),
-  ('Century Tower',        29.6480, -82.3437, 'University Ave, Gainesville, FL 32611', NULL),
-  ('Lake Alice',           29.6423, -82.3612, 'Museum Rd, Gainesville, FL 32611',      NULL),
-  ('The Swamp Restaurant', 29.6515, -82.3249, '1642 W University Ave, Gainesville, FL', NULL);
+  (10, 'Turlington Rock', 29.6491, -82.3438, 'Turlington Hall, Newell Dr, Gainesville, FL 32611', NULL),
+  (11, 'French Fries', 29.6481, -82.3438, 'Marston Science Library, Gainesville, FL 32611', NULL),
+  (12, 'Gator Ubiquity', 29.6515, -82.3400, '1325 W University Ave, Gainesville, FL 32601', Null),
+  (13, 'Gator Corner Dining Center', 29.6481, -82.3503, '114 Rec Ctr, Gainesville, FL 32611', NULL),
+  (14, 'Cravings Campus Kitchen', 29.6501, -82.3477, '114 Fletcher Dr, Gainesville, FL 32611', NULL),
+  (15, 'The Hub', 29.6481, -82.3455, '1765 Stadium Rd, Gainesville, FL 32608', NULL),
+  (16, 'Broward Dining Facility', 29.6465, -82.3420, '60 Inner Rd, Gainesville, FL 32610', NULL),
+  (17,'Century Tower', 29.6480, -82.3437, 'University Ave, Gainesville, FL 32611', NULL),
+  (18,'Lake Alice', 29.6423, -82.3612, 'Museum Rd, Gainesville, FL 32611',      NULL),
+  (19,'The Swamp Restaurant', 29.6515, -82.3249, '1642 W University Ave, Gainesville, FL', NULL);
 
 INSERT INTO traditions (tradition_id, uq_user_tradition_name, description, category, fbook_pagenum) VALUES
   ('Listen to the Chimes of Century Tower', 
