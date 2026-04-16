adcdda672ba43a1271fef7c9d3d30663ce10d48b
Ahilan001 <athirumurugan@ufl.edu>
2026-04-16 11:19:56 -0400

    Added multiple locations support

--- pages/tradition_template.php
+++ pages/tradition_template.php
@@ -1,6 +1,9 @@
 <?php
   $title = "Residence Page";
-  
+
+  $config = require __DIR__ . '/../config.php';
+  $apiKey = $config['GOOGLE_MAPS_API_KEY'] ?? '';
+
   $tradition_id = $_GET['tradition_id'] ?? 1;
   
   $db = new mysqli("localhost", "root", "", "fbook_online");
@@ -13,12 +16,28 @@
   $tradition = $result->fetch_assoc();
 
   $location_query = "SELECT l.latitude, l.longitude, l.place_name, l.address FROM location l JOIN tradition_locations tl ON l.location_id = tl.location_id
-                                                        WHERE tl.tradition_id = ? LIMIT 1"; //limited to 1 location for now
+                                                        WHERE tl.tradition_id = ?";
   $location_stmt = $db->prepare($location_query);
   $location_stmt->bind_param("i", $tradition_id);
   $location_stmt->execute();
   $location_result = $location_stmt->get_result();
-  $location = $location_result->fetch_assoc();
+  //$location = $location_result->fetch_assoc();
+  $locations = [];
+  while ($row = $location_result->fetch_assoc()) {
+      $locations[] = $row;
+  }
+
+  $avgLat = 0;
+  $avgLong = 0;
+  $count = count($locations);
+
+  foreach ($locations as $loc) {
+      $avgLat += (float)$loc['latitude'];
+      $avgLong += (float)$loc['longitude'];
+  }
+
+  $avgLat /= $count;
+  $avgLong /= $count;
 ?>
 <html>
 	<head>
@@ -30,7 +49,7 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 		<link rel="stylesheet" href="../css/residence_page_style.css">
         <script
-                src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAEjhttWwqCpQkmehZFGTpjoKSDII-XhZk&libraries=maps,marker&v=weekly&internal_usage_attribution_ids=gmp_git_jsapisamples_v1_web-components"
+                src="https://maps.googleapis.com/maps/api/js?key=<?php echo htmlspecialchars($apiKey); ?>&libraries=maps,marker&v=weekly"
                 defer>
         </script>
 	</head>
@@ -128,12 +147,14 @@
                     <img src="../assets/residence_hall_placeholder_img.png" class="border border-secondary border-5" alt="UF Logo" style="height: 20%">
                 </div>
 
-                <gmp-map center="<?php echo $location['latitude']; ?>,<?php echo $location['longitude']; ?>" zoom="13"
+                <gmp-map center="<?php echo $avgLat; ?>,<?php echo $avgLong; ?>,<?php echo $locations[0]['longitude']; ?>" zoom="13"
                          style="width:100%;height:280px;border-radius:8px;margin-top:12px;">
-                    <gmp-advanced-marker
-                            position="<?php echo $location['latitude']; ?>,<?php echo $location['longitude']; ?>"
-                            title="<?php echo htmlspecialchars($location['place_name']); ?>">
-                    </gmp-advanced-marker>
+                    <?php foreach ($locations as $location): ?>
+                        <gmp-advanced-marker
+                                position="<?php echo $location['latitude']; ?>,<?php echo $location['longitude']; ?>"
+                                title="<?php echo htmlspecialchars($location['place_name']); ?>">
+                        </gmp-advanced-marker>
+                    <?php endforeach; ?>
                 </gmp-map>
 			</div>
 		</div>
